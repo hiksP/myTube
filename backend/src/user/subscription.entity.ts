932114896b4d3eeb977d32956da728cd0e3c74f4
Plavskikh Peter <qksyo@yandex.ru>
2023-04-05 18:10:28 +0700

    backend + entities

---
+++ backend/src/user/subscription.entity.ts
@@ -0,0 +1,15 @@
+import { Base } from 'src/utils/base'
+import { VideoEntity } from 'src/video/video.entity'
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm'
+import { UserEntity } from './user.entity'
+
+@Entity('Subscription')
+export class SubscriptionEntity extends Base {
+  @ManyToOne(() => UserEntity, user => user.subscriptions)
+  @JoinColumn({ name: 'from_user_id' })
+  fromUser: UserEntity
+
+  @ManyToOne(() => UserEntity, user => user.subscriptions)
+  @JoinColumn({ name: 'to_channel_id' })
+  toChanel: VideoEntity[]
+}
